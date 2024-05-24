b51d5ddb7bbf868f484380b4f5a820e42c3444a2
Vee227 <157512338+Vee227@users.noreply.github.com>
2024-05-24 16:17:36 +0200

    TC3_Blue_Granig

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -3,9 +3,9 @@ ElectronGame.prototype.add = function(array_numbers) {
     const split_array = array_numbers.split(',').map(Number);
     let sum = 0;
 
-	if(split_array.length == 6 && split_array[0] == split_array[1])
+	if(split_array.length > 5 && split_array[0] <= split_array[1])
 	{
-		return split_array[5]-split_array[4];
+		return split_array[split_array.length-1]-split_array[split_array.length-2];
 	} else {
 		for(let i = 0; i < split_array.length; i++){
 			if(split_array[i] % 2 === 0) {
