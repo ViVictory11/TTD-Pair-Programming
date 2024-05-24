b942191912b08ff0552a9326fc27158fb5a77477
Wiktoria Niziolek <wiktoria.niziolek@gmail.com>
2024-05-24 15:46:07 +0200

    TC2_Red_Niziolek

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -6,4 +6,8 @@ describe('Electron Game', function() {
 	it('should return 6 for 1, 2, 3, 4, 5', function() {
 		expect(game.add('1, 2, 3, 4, 5')).toEqual(6);
 	});
+
+	it('should return 4 for 2, 2, 3, 3', function() {
+		expect(game.add('2, 2, 3, 3')).toEqual(4);
+	});
 });
