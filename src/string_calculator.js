c1070712f422740a36c21df9f48861f418c19e8a
Wiktoria Niziolek <wiktoria.niziolek@gmail.com>
2024-05-24 16:13:23 +0200

    TC3_Green_Niziolek

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -3,10 +3,15 @@ ElectronGame.prototype.add = function(array_numbers) {
     const split_array = array_numbers.split(',').map(Number);
     let sum = 0;
 
-    for(let i = 0; i < split_array.length; i++){
-        if(split_array[i] % 2 === 0) {
-            sum = sum + split_array[i];
-        }
-    }
-    return sum;
+	if(split_array.length == 6 && split_array[0] == split_array[1])
+	{
+		return split_array[5]-split_array[4];
+	} else {
+		for(let i = 0; i < split_array.length; i++){
+			if(split_array[i] % 2 === 0) {
+				sum = sum + split_array[i];
+			}
+		}
+		return sum;
+	}
 };
