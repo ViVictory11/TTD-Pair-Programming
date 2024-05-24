ce0832078d648ce1a66526d5614139bf7013d8ba
Vee227 <157512338+Vee227@users.noreply.github.com>
2024-05-24 16:22:03 +0200

    TC4_Green_Granig

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -2,16 +2,13 @@ function ElectronGame() {}
 ElectronGame.prototype.add = function(array_numbers) {
     const split_array = array_numbers.split(',').map(Number);
     let sum = 0;
+	let counter = 0;
 
-	if(split_array.length > 5 && split_array[0] <= split_array[1])
-	{
-		return split_array[split_array.length-1]-split_array[split_array.length-2];
-	} else {
-		for(let i = 0; i < split_array.length; i++){
-			if(split_array[i] % 2 === 0) {
-				sum = sum + split_array[i];
-			}
+	for(let i = 0; i < split_array.length; i++){
+		if(split_array[i] % 2 != 0){
+			sum = sum + split_array[i];
+			counter++;
 		}
-		return sum;
 	}
+	return sum - counter;
 };
